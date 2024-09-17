0fd2a70917991f2071a85f1cfd95039cc14f9757
Laurynas Jagutis <laurynas.jagutis@alliander.com>
2024-09-17 10:55:51 +0300

    fix up failing checks
    Signed-off-by: Laurynas Jagutis <laurynas.jagutis@alliander.com>

--- power_grid_model_io_native_c/power_grid_model_io_native/include/power_grid_model_io_native/vnf_converter/vnf_pgm_converter.hpp
+++ power_grid_model_io_native_c/power_grid_model_io_native/include/power_grid_model_io_native/vnf_converter/vnf_pgm_converter.hpp
@@ -58,7 +58,7 @@ inline void PgmVnfConverter::parse_vnf_file(){
     // will be implemented later
 };
 
-inline power_grid_model::ConstDataset* PgmVnfConverter::convert_input(power_grid_model::ConstDataset* dataset) {
+inline power_grid_model::ConstDataset* PgmVnfConverter::convert_input(power_grid_model::ConstDataset* /*dataset*/) {
     convert_node_input();
     convert_line_input();
     convert_sources_input();
